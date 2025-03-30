aaba3b12342150aea33dfc63dbb388e289853aef
Lewis Allan <lewis.allan1992@hotmail.com>
2025-03-30 20:44:11 +0100

    expand on logging with context helper

--- zaphelper/zap.go
+++ zaphelper/zap.go
@@ -39,3 +39,34 @@ func FromContext(ctx context.Context) *zap.Logger {
 	}
 	return ZapLogger
 }
+
+// With will add l to the context
+func With(ctx context.Context, logger *zap.Logger) context.Context {
+	return context.WithValue(ctx, struct{}{}, logger)
+}
+
+// Error will log at the error level using the associated context
+func Error(ctx context.Context, msg string, fields ...zap.Field) {
+	FromContext(ctx).Error(msg, fields...)
+}
+
+// Info will log at the info level using the associated context
+func Info(ctx context.Context, msg string, fields ...zap.Field) {
+	FromContext(ctx).Info(msg, fields...)
+}
+
+// Warn will log at the warn level using the associated context
+func Warn(ctx context.Context, msg string, fields ...zap.Field) {
+	FromContext(ctx).Warn(msg, fields...)
+}
+
+// Debug will log at the debug level using the associated context
+func Debug(ctx context.Context, msg string, fields ...zap.Field) {
+	FromContext(ctx).Debug(msg, fields...)
+}
+
+// Sync calls the underlying Core's Sync method, flushing any buffered log entries.  Applications should take care to call
+// Sync before exiting.
+func Sync(ctx context.Context) error {
+	return FromContext(ctx).Sync()
+}
